f28a539fc013378626545d76bfdf190804ba5e08
Julian Elve <julian.elve@gmail.com>
2017-02-09 09:58:24 +0000

    better logic around styles

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ function syn_enqueue_styles() {
     $parent_style = 'twentyseventeen-style';
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
+        get_stylesheet_directory_uri() . '/assets/css/style.css',
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
